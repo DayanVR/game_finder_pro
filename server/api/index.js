a453bca0f4bfc0eac306d361fd956f93c6d63e0e
DayanVR <dayanvillavicencio14@gmail.com>
2024-04-29 19:47:11 -0400

    i

--- server/api/index.js
+++ server/api/index.js
@@ -11,28 +11,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post("/game", async (req, res) => {
-  try {
-    const { fields, where } = req.body;
-
-    let query = `fields ${fields};`;
-    const headers = {
-      Accept: "application/json",
-      Authorization: "Bearer xgquzw1xe7xvsaway0v5sih2mbc0yi",
-      "Client-ID": "w3digq04cfa0r0n86enjwuwn3ci1hk",
-    };
-
-    const response = await axios.post("https://api.igdb.com/v4/games", query, {
-      headers,
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 app.listen(PORT, () => console.log("Server ready on port 4000."));
 
 app.post("/api/games", async (req, res) => {
@@ -85,6 +63,7 @@ app.post("/api/details", async (req, res) => {
 });
 
 module.exports = app;
+
 
 /*app.use("/.netlify/functions/server", app);
 export const handler = serverless(app);*/
