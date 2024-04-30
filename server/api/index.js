4bcaa64ab3a93e0a937d932ecaa223bd1016876b
DayanVR <dayanvillavicencio14@gmail.com>
2024-04-30 13:40:01 -0400

    done

--- server/api/index.js
+++ server/api/index.js
@@ -1,53 +1,26 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const serverless = require("serverless-http");
-const http = require("node:http");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors()); // Enable CORS for all routes
+const corsOptions = {
+  origin: "https://game-finder-pro.netlify.app",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization", "Client-ID"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Render"));
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
-/*app.post("/api", async (req, res) => {
-  try {
-    const { fields, where, limit, offset, sort, search } = req.body;
-    
-    let query = `fields ${fields}; where ${where}; limit ${limit}; offset ${offset};`;
-    if (search && search.length > 0) {
-      query += ` search "${search}";`;
-    } else {
-      query += ` sort ${sort};`;
-    }
-    
-    console.log(fields);
-    console.log(query);
-    console.log(req.body);
-
-    const headers = {
-      Accept: "application/json",
-      Authorization: "Bearer xgquzw1xe7xvsaway0v5sih2mbc0yi",
-      "Client-ID": "w3digq04cfa0r0n86enjwuwn3ci1hk",
-    };
-    const response = await axios.post("https://api.igdb.com/v4/games", query, {
-      headers,
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});*/
-
 app.post("/api/games", async (req, res) => {
   try {
     const { fields, where, limit, offset, sort, search } = req.body;
-    
+
     let query = `fields ${fields}; where ${where}; limit ${limit}; offset ${offset};`;
     if (search && search.length > 0) {
       query += ` search "${search}";`;
@@ -59,12 +32,11 @@ app.post("/api/games", async (req, res) => {
       Authorization: "Bearer xgquzw1xe7xvsaway0v5sih2mbc0yi",
       "Client-ID": "w3digq04cfa0r0n86enjwuwn3ci1hk",
     };
-    console.log(req.body)
-    
+    console.log(req.body);
+
     const response = await axios.post("https://api.igdb.com/v4/games", query, {
       headers,
     });
-    
 
     res.status(201).json(response.data);
   } catch (error) {
@@ -95,6 +67,3 @@ app.post("/api/details", async (req, res) => {
 });
 
 module.exports = app;
-
-/*app.use("/.netlify/functions/server", app);
-export const handler = serverless(app);*/
