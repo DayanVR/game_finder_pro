03fb5986b4f92f5d3a2ef0726aca6a9ddd4a0c4f
DayanVR <dayanvillavicencio14@gmail.com>
2024-05-04 13:31:19 -0400

    flex to header

--- client/src/components/games_Info/GameDetailsPage.js
+++ client/src/components/games_Info/GameDetailsPage.js
@@ -147,7 +147,7 @@ function GameDetailsPage() {
               </button>
             </div>
 
-            <div>
+            <div className="flex">
               {isGameSaved ? (
                 <button
                   className="flex w-fit cursor-pointer items-center space-x-2 rounded-md px-3 py-2 hover:bg-slate-800/[0.8] max-lg:mx-auto"
