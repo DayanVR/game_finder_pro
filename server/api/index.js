e6272239c9e1ea63ccbcccdf924114742a1b895b
DayanVR <dayanvillavicencio14@gmail.com>
2024-05-09 15:57:44 -0400

    all

--- server/api/index.js
+++ server/api/index.js
@@ -12,20 +12,35 @@ app.use(helmet());
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: "https://gamefinderpro.netlify.app",
+  origin: "*",
   methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "Authorization", "Client-ID"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Client-ID",
+  ],
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) =>
+  res.send("Express on Vercel")
+);
 
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+app.listen(PORT, () =>
+  console.log(`Server ready on port ${PORT}.`)
+);
 
 app.post("/api/games", async (req, res) => {
   try {
-    const { fields, where, limit, offset, sort, search } = req.body;
+    const {
+      fields,
+      where,
+      limit,
+      offset,
+      sort,
+      search,
+    } = req.body;
 
     let query = `fields ${fields}; where ${where}; limit ${limit}; offset ${offset};`;
     if (search && search.length > 0) {
@@ -38,37 +53,49 @@ app.post("/api/games", async (req, res) => {
       Authorization: authorization,
       "Client-ID": clientId,
     };
-    
-    const response = await axios.post("https://api.igdb.com/v4/games", query, {
-      headers,
-    });
-    
+
+    const response = await axios.post(
+      "https://api.igdb.com/v4/games",
+      query,
+      {
+        headers,
+      }
+    );
+
     res.status(201).json(response.data);
   } catch (error) {
     console.error("Error:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res
+      .status(500)
+      .json({ error: "Internal server error" });
   }
 });
 
 app.post("/api/details", async (req, res) => {
   try {
     const { fields, where } = req.body;
-    
+
     let query = `fields ${fields}; where ${where};`;
     const headers = {
       Accept: "application/json",
       Authorization: authorization,
       "Client-ID": clientId,
     };
-    
-    const response = await axios.post("https://api.igdb.com/v4/games", query, {
-      headers,
-    });
-    
+
+    const response = await axios.post(
+      "https://api.igdb.com/v4/games",
+      query,
+      {
+        headers,
+      }
+    );
+
     res.json(response.data);
   } catch (error) {
     console.error("Error:", error.message);
-    res.status(500).json({ error: error.message });
+    res
+      .status(500)
+      .json({ error: error.message });
   }
 });
 
