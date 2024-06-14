52c06ef3aa7fcadb65500391b293d1bc7f3556a3
Dayan Rolffot <dayanvillavicencio13@gmail.com>
2024-06-14 11:46:54 -0400

    fixed acces

--- client/src/components/functions/Selection.js
+++ client/src/components/functions/Selection.js
@@ -3,18 +3,24 @@ import React from "react";
 function Selection(props) {
   return (
     <div className="flex max-md:mx-auto max-md:flex-col max-md:space-y-4 md:justify-between md:space-x-4 lg:space-x-6 ">
+      <label htmlFor="sorting">Sort by:</label>
       <select
         onChange={props.handleOrderChange}
         disabled={props.searchGame}
         className="rounded-md px-2 py-2.5 md:px-6"
+        id="sorting"
       >
-        <option value="none">Order by: Rating</option>
-        <option value="order-asc">Rating ASC</option>
-        <option value="name-asc">Name: A to Z</option>
-        <option value="name-desc">Name: Z to A</option>
+        <optgroup label="Rating:">
+          <option value="none">DESC</option>
+          <option value="order-asc">ASC</option>
+        </optgroup>
+        <optgroup label="Name:">
+          <option value="name-asc">A to Z</option>
+          <option value="name-desc">Z to A</option>
+        </optgroup>
       </select>
       <select
-        className="rounded-md xl:hidden px-2 py-2.5 md:px-6"
+        className="rounded-md px-2 py-2.5 md:px-6 xl:hidden"
         onChange={props.handlePlatformChange}
       >
         <option value="0">All Platforms</option>
